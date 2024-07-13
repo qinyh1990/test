7dca371a3b5b489e977b598a1b616bbc2147d42f
qinyh1990 <45475895+qinyh1990@users.noreply.github.com>
2024-07-13 09:58:55 +0800

    Create io.sh

---
+++ io.sh
@@ -0,0 +1,3 @@
+source /etc/profile
+cd /root
+./io_net_launch_binary_linux --device_id=2f031602-91fb-4e61-adfb-f8b665a422bd --user_id=5f05a238-bbc5-4e0b-b764-3b5eef26d74a --operating_system="Linux" --usegpus=true --device_name="New-device" --disable_sleep_mode=true
